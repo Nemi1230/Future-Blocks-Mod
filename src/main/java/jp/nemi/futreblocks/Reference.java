c87114f730da0aabf7e756b1b3d289800b19b4ae
Nemi1230 <nemiakuma@gmail.com>
2022-10-30 19:50:15 +0900

    initial commit

---
+++ src/main/java/jp/nemi/futreblocks/Reference.java
@@ -0,0 +1,8 @@
+package jp.nemi.futreblocks;
+
+public class Reference {
+    public static final String MOD_ID = "futureblocks";
+    public static final String MOD_NAME = "Future Blocks Mod";
+    public static final String MOD_VERSION = "1.12.2-1A0N0";
+    public static final String MC_VERSION = "Minecraft1.12.2";
+}
